11a4d2e144e318b3a9c5631477c8b3ace2100f82
mge <mge@8988ead5-cfd4-0310-b5c8-f7b9d52f4d72>
2006-03-23 22:36:09 +0000

    reduce size for si_readfile -> faster(?)

--- trunk/sitar/pfrags/functions.sitar/si_readfile.pl
+++ trunk/sitar/pfrags/functions.sitar/si_readfile.pl
@@ -1,18 +1,14 @@
 #
 # si_readfile( filename )
 #
+# read small(!) files below /proc, ...
+#
 sub si_readfile( $ ) {
         my ( $filename ) = shift( @_ );
 	my $content;
-	my $readsize;
-	if( $SITAR_OPT_LIMIT > 0 ) { 
-		$readsize = $SITAR_OPT_LIMIT;
-	} else {
-		$readsize = $SITAR_MAX_LIMIT;
-	}
 	if( -r $filename ) {
 		if( open( $HANDLE , "< $filename" ) ) {
-			read( $HANDLE, $content, $readsize );
+			read( $HANDLE, $content, $SITAR_READFILE_LIMIT );
 			close( $HANDLE );
 		}
 	}
