bea1f0a88d0baedf0d1c2be93a1ca73c886cc099
mge <mge@8988ead5-cfd4-0310-b5c8-f7b9d52f4d72>
2006-11-22 11:22:06 +0000

    additional checks

--- trunk/sitar/pfrags/sitar.globals.pl
+++ trunk/sitar/pfrags/sitar.globals.pl
@@ -64,10 +64,10 @@ chomp( my $CMD_SORT      = `which sort` );
 chomp( my $CMD_SORTUNIQ  = "$CMD_SORT -u" );
 chomp( my $CMD_TUNE2FS   = `which tune2fs` );
 chomp( my $CMD_UNAME     = `which uname` );
-# should also search for cpqacucli
 chomp( my $CMD_ACUCLI    = `which hpacucli` );
+chomp( my $CMD_CPQACUCLI = `which cpqacucli` );
 chomp( my $CMD_DMIDECODE = `which dmidecode` );
-# should also serach for cli32
+# cli32 is searched for in the function, ...
 chomp( my $CMD_ARECACLI  = "/usr/lib/snmp/cli64" );
 open( STDERR, ">&SAVEERR" );
 $ENV{ PATH } = '';
